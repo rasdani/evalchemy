14c11f7a78933d0ae4aded87a1a0f9dfac913560
rasdani <73563550+rasdani@users.noreply.github.com>
2025-06-19 22:59:20 +0000

    lower concurrency

--- qwen3_vllm_oai.sh
+++ qwen3_vllm_oai.sh
@@ -2,7 +2,7 @@ export OPENAI_API_KEY=EMPTY
 
 python -m eval.eval \
     --model openai-chat-completions \
-    --tasks MBPP \
-    --model_args "model=Qwen/Qwen3-1.7B,num_concurrent=64,base_url=http://localhost:8000/v1/chat/completions" \
-    --batch_size 16 \
+    --tasks SWEbench \
+    --model_args "model=rasdani/git-diff-Qwen-1.7B,num_concurrent=32,base_url=http://localhost:8000/v1/chat/completions" \
+    --apply_chat_template \
     --output_path logs
